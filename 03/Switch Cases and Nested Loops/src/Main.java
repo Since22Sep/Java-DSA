f224fbe7a7c2f2e3ed31d7337256ecea59d3dfa2
Dipanshu <dipanshukumar344@gmail.com>
2025-11-11 15:51:37 +0530

    Switch Statements

---
+++ 03/Switch Cases and Nested Loops/src/Main.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+//        String fruit = in.next();
+//
+//        switch(fruit){
+//            case "Mango" -> System.out.println("King of fruits");
+//            case "Apple" -> System.out.println("A sweet red fruit");
+//            case "Orange" -> System.out.println("Round fruit");
+//            case "Grapes" -> System.out.println("Small fruit");
+//            default -> System.out.println("Please enter a valid fruit");
+//
+//
+//        }
+
+//        int day = in.nextInt();
+//        switch(day){
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesdsay");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//        }
+
+
+
+    }
+}
